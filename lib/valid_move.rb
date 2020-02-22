7de367cd22f88646845daaf3f81c02abbfff7f35
rouan.thompson <rouan.thompson@gmail.com>
2020-02-22 04:05:52 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,17 @@
 # code your #valid_move? method here
-
+def valid_move?(array, index)
+  if index.between(1, 9) && position_taken?(array, index)
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(array, index)
+  if array[index] == " "
+    false
+  elsif array[index] == ""
+    false
+  elsif array[index] == nil
+    false
+  else
+    true
+  end
+end
