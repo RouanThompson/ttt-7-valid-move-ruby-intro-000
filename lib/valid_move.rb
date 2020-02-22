0d42a0d351b70ccf06bf062a2dc18d5bb8450b01
rouan.thompson <rouan.thompson@gmail.com>
2020-02-22 04:15:04 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 def valid_move?(array, index)
-  if index.between?(1, 9) == true && position_taken?(array, index) == false
+  if index.between?(0, 8) == true && position_taken?(array, index) == false
     true
   else
     false
